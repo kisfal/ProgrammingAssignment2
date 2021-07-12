5d83297922f41a91d9926851e66bc8f4fc49f15e
kisfal <marton.kisfaludi@gmail.com>
2021-07-12 18:54:52 +0200

    R Programming assignment of week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+##This program is useful if we want to calculate the inverse of a matrix multiple times.
 
-## Write a short comment describing this function
-
+## Makes a special matrix type
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inver) inv <<- inver
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## This function return the inverse of the previously defined special matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  print(data)
+  inv <- solve(data)
+  x$setinv(inv)
+  inv
 }
